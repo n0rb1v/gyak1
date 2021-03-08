17f52605294e5d375248277e0ee844078581e2b8
n0rb1v <n0rb1v@gmail.com>
2021-03-08 17:15:47 +0100

    2

--- src/main/java/CdvCheck.java
+++ src/main/java/CdvCheck.java
@@ -1,17 +1,17 @@
 public class CdvCheck {
     public boolean check(String s) {
-        if (s.length()!=10){
+        if (s.length() != 10) {
             throw new IllegalArgumentException("hibas szam");
         }
         int sum = 0;
         try {
             for (int i = 0; i < 9; i++) {
-                sum += Integer.parseInt(String.valueOf(s.charAt(i))) * (i+1);
+                sum += Integer.parseInt(String.valueOf(s.charAt(i))) * (i + 1);
             }
             if (Integer.parseInt(String.valueOf(s.charAt(9))) == sum % 11) {
                 return true;
             }
-        }catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException("hibas parameter");
         }
         return false;
--- src/main/java/OwlCounter.java
+++ src/main/java/OwlCounter.java
@@ -1,11 +1,21 @@
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
 
 public class OwlCounter {
-    public void readFromFile(BufferedReader reader) {
+    private Map<String, Integer> data = new HashMap<>();
 
+    public void readFromFile(BufferedReader reader) throws IOException {
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] temp = line.split("=");
+            data.put(temp[0], Integer.parseInt(temp[1]));
+        }
     }
 
     public int getNumberOfOwls(String s) {
-        return 0;
+        return data.get(s);
     }
 }
