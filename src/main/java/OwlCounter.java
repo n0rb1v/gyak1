02bd0e37e4f52a04eeff75cef8f4b0b0c45721d1
n0rb1v <n0rb1v@gmail.com>
2021-03-08 17:04:34 +0100

    1

---
+++ src/main/java/OwlCounter.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+
+public class OwlCounter {
+    public void readFromFile(BufferedReader reader) {
+
+    }
+
+    public int getNumberOfOwls(String s) {
+        return 0;
+    }
+}
