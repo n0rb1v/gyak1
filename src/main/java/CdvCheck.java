0fc6ae1770e8c09e4b93d7c61c647722f3366bca
n0rb1v <n0rb1v@gmail.com>
2021-03-08 14:35:58 +0100

    2

--- src/main/java/CdvCheck.java
+++ src/main/java/CdvCheck.java
@@ -1,5 +1,19 @@
 public class CdvCheck {
     public boolean check(String s) {
-
+        if (s.length()!=10){
+            throw new IllegalArgumentException("hibas szam");
+        }
+        int sum = 0;
+        try {
+            for (int i = 0; i < 9; i++) {
+                sum += Integer.parseInt(String.valueOf(s.charAt(i))) * (i+1);
+            }
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException("hibas parameter");
+        }
+        if (Integer.parseInt(String.valueOf(s.charAt(9))) == sum % 11) {
+            return true;
+        }
+        return false;
     }
 }
